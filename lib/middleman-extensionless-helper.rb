1814b4a7a74e000b85993b7d738bde826cf69ba7
AT-AT <dec.alpha21264@gmail.com>
2016-04-14 10:56:25 +0900

    Wrap class in module

--- lib/middleman-extensionless-helper.rb
+++ lib/middleman-extensionless-helper.rb
@@ -2,5 +2,5 @@ require 'middleman-core'
 
 ::Middleman::Extensions.register(:extensionless_helper) do
   require 'middleman-extensionless-helper/extension'
-  ::Middleman::ExtensionLessHelper
+  ::Middleman::ExtensionLessHelper::Extension
 end
